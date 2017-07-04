bc98fb5dec363e20911ffc3ed459a5fc992dbd84
ren_xt <allbutone@foxmail.com>
2017-07-04 21:15:20 +0800

    add in-memory user and roles for test purposes

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -16,5 +17,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
             .formLogin()//默认 username 为 "user", password 从启动日志中查找
                 .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+//                spring security 会自动为角色添加 "ROLE_" 前缀
+                .withUser("tom").password("tom").roles("ADMIN", "USER")
+                .and()
+                .withUser("jack").password("jack").roles("USER")
+                .and()
+                .withUser("locked").password("locked").roles("USER").accountLocked(true)
+                .and()
+                .withUser("disabled").password("disabled").roles("USER").disabled(true);
     }
 }
