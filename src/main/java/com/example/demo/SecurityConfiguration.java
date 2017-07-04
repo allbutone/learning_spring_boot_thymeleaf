3be49c3776f9e66ef698486c4a23ac93773b8b8c
ren_xt <allbutone@foxmail.com>
2017-07-04 21:01:43 +0800

    添加 spring security + thymeleaf 实现的 form login

---
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by ren_xt
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()//默认 username 为 "user", password 从启动日志中查找
+                .permitAll();
+    }
+}
